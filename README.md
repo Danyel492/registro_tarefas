52272eace802a5c7415dfe7cd1613a40f3f3f166
Danyel492 <danyelguto@gmail.com>
2024-09-01 11:37:44 -0300

    pagina de print - estilização

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 /*
 PROJETO DE REGISTRO DE ATIVIDADES
 */
+Propósito do projeto:
+O projeto de registro de atividades foi desenvolvido baseado em uma demanda de desenvolver relatórios diários de tarefas executas.
+Todos os dias precisava descrever cada atividade executada durante o dia, bem como as paradas e o que fazia entre projetos.
+Essa aplicação facilita a entrada de dados fornecendo uma ordem de tarefas executadas juntamento com os horarios de cada ponto na timeline.
+Ao final do dia é gerado um relatorio completa para ser enviado por email.
+--
+Ultima atualização: 01/09/2024
+*Atualizações na pagina de impressão
+*Estilização dos botões
+
+Implementações futuras:
+*Integração com IA para gerar texto do relatório a ser enviado, personalizando para cada usuário
+*Registro em um bando de dados
+*Criação de usuário
--- scripts.js
+++ scripts.js
@@ -1,31 +1,36 @@
 function adicionar() {
-    const tarefas = document.querySelector('ul')
-    const registro = document.querySelector('#registro')
 
-    const novaTimeline = document.createElement('li')
-    novaTimeline.classList.add('timeline')
+    if (document.querySelector('#registro').value === '') {
+        alert('Você precisa digitar uma tarefa antes de adiciona-la')
+    } else {
+        const tarefas = document.querySelector('ul')
+        const registro = document.querySelector('#registro')
 
-    const tarefa = document.createElement('span')
-    tarefa.textContent = registro.value
+        const novaTimeline = document.createElement('li')
+        novaTimeline.classList.add('timeline')
 
-    const dataHora = document.createElement('span')
-    const dataAtual = new Date()
-    const options = {
-        year: 'numeric',
-        month: 'numeric',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric'
+        const tarefa = document.createElement('span')
+        tarefa.textContent = registro.value
+
+        const dataHora = document.createElement('span')
+        const dataAtual = new Date()
+        const options = {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric'
+        }
+        dataHora.textContent = dataAtual.toLocaleDateString('pt-BR', options)
+
+        novaTimeline.appendChild(tarefa)
+        novaTimeline.appendChild(dataHora)
+        tarefas.appendChild(novaTimeline)
+
+        registro.value = ''
     }
-    dataHora.textContent = dataAtual.toLocaleDateString('pt-BR', options)
-
-    novaTimeline.appendChild(tarefa)
-    novaTimeline.appendChild(dataHora)
-    tarefas.appendChild(novaTimeline)
-
-    registro.value = ''
 }
-document.querySelector('#registro').addEventListener('keypress', function(event) {
+document.querySelector('#registro').addEventListener('keypress', function (event) {
     if (event.key === 'Enter') {
         event.preventDefault()
         adicionar()
